62552a6bf8105ad5451eeb20a04e7243421754ae
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2020-10-05 02:12:10 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <div className="pt-10 pb-5 bg-white">
       <div className="container flex justify-center pl-56 mx-auto">
         <p className="text-sm">
-          © Copyrigth 2020{' '}
+          © Copyright 2020{' '}
           <a
             href="https://www.linkedin.com/in/gustavo-da-silva-ramos-9b1433169/"
             className="font-semibold text-primary-600 hover:underline"
