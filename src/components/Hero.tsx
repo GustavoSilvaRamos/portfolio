ebb19904d3739558e4a104e582fb6e637d726011
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2020-10-06 15:48:16 -0300

    Update navbar and typography

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,18 @@ export function Hero() {
     <div id="hero" className="relative min-h-screen fundo">
       <div className="absolute z-10 w-full h-full bg-black opacity-50" />
       <div className="container fixed z-20 flex flex-col w-2/12 h-screen bg-black ">
-        <div className="flex flex-col items-center pt-10">
-          <img
-            className="h-40 border-4 rounded-full border-primary-600"
-            src="perfil.jpg"
-            alt="Perfil"
-          />
-          <p className="pt-3 text-2xl text-gray-300">Gustavo Silva</p>
-          <ul className="flex flex-col items-center pt-16 space-y-3 text-base text-gray-300">
+        <div className="flex flex-col items-center justify-around h-screen ">
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className="h-40 border-4 rounded-full border-primary-600"
+              src="perfil.jpg"
+              alt="Perfil"
+            />
+            <p className="pt-3 text-2xl font-medium text-gray-300">
+              Gustavo Silva
+            </p>
+          </div>
+          <ul className="flex flex-col items-center pt-16 space-y-3 text-lg tracking-wide text-gray-300">
             <a
               className="transition duration-300 ease-in-out hover:text-primary-600"
               href="#hero"
@@ -27,24 +31,24 @@ export function Hero() {
           <div className="flex flex-row justify-around pt-48 space-x-5">
             <a
               href="https://www.linkedin.com/in/gustavo-da-silva-ramos-9b1433169/"
-              className="text-lg text-gray-300 transition duration-300 ease-in-out fab fa-linkedin hover:text-primary-600"
+              className="text-lg text-gray-300 transition duration-300 ease-in-out fab fa-linkedin hover:text-primary-400"
             />
             <a
               href="https://github.com/GustavoSilvaRamos"
-              className="text-lg text-gray-300 transition duration-300 ease-in-out fab fa-github hover:text-primary-600"
+              className="text-lg text-gray-300 transition duration-300 ease-in-out fab fa-github hover:text-gray-600"
             />
           </div>
         </div>
       </div>
       <div className="z-20 flex flex-col items-center pt-48 pl-56 text-white">
         <h2 className="z-20 text-3xl font-medium">Welcome</h2>
-        <h1 className="z-20 text-6xl font-medium tracking-normal">
-          I'm a Designer
+        <h1 className="z-20 text-6xl font-medium tracking-normal ">
+          I'm a Jr. Developer
         </h1>
         <h3 className="z-20 pt-2 pb-8 text-xl font-normal">
           based in Mirassol, São Paulo
         </h3>
-        <button className="z-20 px-5 py-3 font-medium tracking-wide transition duration-200 ease-in-out border-2 rounded-full text-primary-600 border-primary-600 hover:text-white hover:bg-primary-600">
+        <button className="z-20 px-5 py-3 font-semibold tracking-wide transition duration-200 ease-in-out border-2 rounded-full text-primary-600 border-primary-600 hover:text-white hover:bg-primary-600">
           <a href="#portfolio">See My Projects</a>
         </button>
       </div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,400;1,700;1,900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,300;1,400&display=swap"
             rel="stylesheet"
           />
         </Head>
