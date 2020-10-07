5da67ade5c661ce88fcdc3def96ceea1b91715b1
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2020-10-06 21:45:35 -0300

    Update Images Of Portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -11,27 +11,39 @@ export default function Portfolio() {
         <div className="container grid grid-flow-row grid-cols-2 gap-8 pt-10 ">
           <div className="transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 hover:opacity-100 hover:border-primary-600">
             <a href="https://architecture-five.vercel.app/">
-              <img className="h-64 " src="HeroArc.jpg" alt="Arquitetura" />
+              <img
+                className="w-full h-64 "
+                src="HeroArc.jpg"
+                alt="Arquitetura"
+              />
             </a>
           </div>
           <div className="transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 hover:opacity-100 hover:border-primary-600">
             <a href="https://maridodealuguel.vercel.app/">
-              <img className="h-64 " src="HeroMDE.jpg" alt="Marido de Alugel" />
+              <img
+                className="w-full h-64"
+                src="HeroMDE.jpg"
+                alt="Marido de Alugel"
+              />
             </a>
           </div>
           <div className="transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 hover:opacity-100 hover:border-primary-600">
             <a href="https://receitas.vercel.app/">
-              <img className="h-64 " src="HeroR.jpg" alt="Receitas" />
+              <img className="w-full h-64" src="HeroR.jpg" alt="Receitas" />
             </a>
           </div>
           <div className="transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 hover:opacity-100 hover:border-primary-600">
             <a href="https://wineryproject.vercel.app/">
-              <img className="h-64 " src="HeroV.jpg" alt="Vinicola" />
+              <img className="w-full h-64" src="HeroV.jpg" alt="Vinicola" />
             </a>
           </div>
           <div className="transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 hover:opacity-100 hover:border-primary-600">
             <a href="https://personaltrainer.vercel.app/">
-              <img className="h-64 " src="HeroPT.jpg" alt="Personal Trainer" />
+              <img
+                className="w-full h-64"
+                src="HeroPT.jpg"
+                alt="Personal Trainer"
+              />
             </a>
           </div>
         </div>
