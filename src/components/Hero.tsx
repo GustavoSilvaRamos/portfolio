ff3095e1a7f54db4df95ce32cd3ef01e0dc7c56e
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2020-10-11 02:39:30 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
   return (
     <div id="hero" className="relative min-h-screen fundo">
       <div className="absolute z-10 w-full h-full bg-black opacity-50" />
-      <div className="z-20 flex flex-col items-center pt-48 ">
-        <h2 className="z-20 text-3xl font-medium text-gray-200">Welcome</h2>
+      <div className="z-20 flex flex-col items-center justify-center h-screen ">
+        <h2 className="z-20 text-3xl font-medium text-gray-400">Welcome</h2>
         <h1 className="z-20 flex text-6xl font-medium tracking-normal text-gray-100 ">
           I'm a
           <span className="ml-6">
@@ -24,7 +24,7 @@ export default function Hero() {
             />
           </span>
         </h1>
-        <h3 className="z-20 pt-2 pb-8 text-xl font-normal text-gray-200">
+        <h3 className="z-20 pt-2 pb-8 text-xl font-normal text-gray-500">
           based in Mirassol, São Paulo
         </h3>
         <a className="z-20" href="#portfolio">
