28c2c3652b7c85c41d35fe555ed83040961f1915
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2021-01-04 19:13:42 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
               site_name: 'Gustavo Silva',
               images: [
                 {
-                  url: '/Open-Graph.png',
+                  url: '/open-graph.png',
                 },
               ],
             }}
