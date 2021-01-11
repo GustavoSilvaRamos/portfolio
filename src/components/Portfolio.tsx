0640f6ef12379a7cac95a70ea55f1520452d06a7
GustavoSilvaRamos <gustavosramos1@hotmail.com>
2021-01-11 19:29:03 -0300

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -11,46 +11,46 @@ export default function Portfolio() {
           </h4>
         </div>
         <div className="container grid grid-flow-row grid-cols-1 gap-8 pt-10 md:grid-cols-2 ">
-          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-64 h-96 hover:opacity-100 hover:border-primary-600">
+          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-80 h-96 hover:opacity-100 hover:border-primary-600">
             <a href="https://architecture-five.vercel.app/">
               <img
-                className="w-full h-96 md:h-64"
+                className="w-full h-96 md:h-80"
                 src="HeroArc.jpg"
                 alt="Arquitetura"
               />
             </a>
           </div>
-          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-64 h-96 hover:opacity-100 hover:border-primary-600">
+          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-80 h-96 hover:opacity-100 hover:border-primary-600">
             <a href="https://maridodealuguel.vercel.app/">
               <img
-                className="w-full h-96 md:h-64 "
+                className="w-full h-96 md:h-80 "
                 src="HeroMDE.jpg"
                 alt="Marido de Alugel"
               />
             </a>
           </div>
-          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-64 h-96 hover:opacity-100 hover:border-primary-600">
+          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-80 h-96 hover:opacity-100 hover:border-primary-600">
             <a href="https://receitas.vercel.app/">
               <img
-                className="w-full h-96 md:h-64"
+                className="w-full h-96 md:h-80"
                 src="HeroR.jpg"
                 alt="Receitas"
               />
             </a>
           </div>
-          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-64 h-96 hover:opacity-100 hover:border-primary-600">
+          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-80 h-96 hover:opacity-100 hover:border-primary-600">
             <a href="https://wineryproject.vercel.app/">
               <img
-                className="w-full h-96 md:h-64"
+                className="w-full h-96 md:h-80"
                 src="HeroV.jpg"
                 alt="Vinicola"
               />
             </a>
           </div>
-          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-64 h-96 hover:opacity-100 hover:border-primary-600">
+          <div className="overflow-hidden transition duration-500 ease-in-out border-4 border-transparent rounded-sm opacity-75 md:h-80 h-96 hover:opacity-100 hover:border-primary-600">
             <a href="https://personaltrainer.vercel.app/">
               <img
-                className="w-full h-96 md:h-64"
+                className="w-full h-96 md:h-80"
                 src="HeroPT.jpg"
                 alt="Personal Trainer"
               />
